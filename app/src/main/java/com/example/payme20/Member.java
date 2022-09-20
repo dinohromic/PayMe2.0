c1325dffc299e376e693c9c95cd55fe15e3b8231
dinohromic <dhromic@gmail.com>
2022-09-20 10:34:21 +0200

    commit

---
+++ app/src/main/java/com/example/payme20/Member.java
@@ -0,0 +1,11 @@
+package com.example.payme20;
+
+public class Member {
+    private String userName;
+    private String phoneNumber;
+
+    public Member(String userName, String phoneNumber){
+        this.userName = userName;
+        this.phoneNumber = phoneNumber;
+    }
+}
