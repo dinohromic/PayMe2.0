81996def7adb4877b7bd20e03c5a6c0fdac213a4
BenjaminTWP <benjamintwpettersson@gmail.com>
2022-09-21 11:44:21 +0200

    Tests makes sure that payer has positive balance when paid for event. Participant has negative debt (to pay)

--- app/src/test/java/com/example/payme20/TestDebtUpdater.java
+++ app/src/test/java/com/example/payme20/TestDebtUpdater.java
@@ -1,36 +1,55 @@
 package com.example.payme20;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TestDebtUpdater {
 
-    @Test
-    public void testSplittingDebt(){
-        Member user1 = new Member("User1", "100");
-        Member user2 = new Member("User2", "200");
-        Member user3 = new Member("User3", "300");
+    Member user1;
+    Member user2;
+    Member user3;
+    Group group;
+    Map<Member, Double> debtMap = new HashMap<Member, Double>();
+    Event testEvent;
 
-        Map<Member, Double> debtMap = new HashMap<Member, Double>();
-        debtMap.put(user1, 330.0);
-        debtMap.put(user2, 0.0);
-        debtMap.put(user3, 0.0);
-
-
-        Group group = new Group("TestGroup");
+    @Before
+    public void init(){
+        user1 = new Member("User1", "100");
+        user2 = new Member("User2", "200");
+        user3 = new Member("User3", "300");
+        group = new Group("TestGroup");
         group.addNewGroupMember(user1);
         group.addNewGroupMember(user2);
         group.addNewGroupMember(user3);
-        Event testEvent = new Event("TestEvent", debtMap, user1, new SplitDebtUpdater());
+        debtMap.put(user1, 330.0);
+        debtMap.put(user2, 0.0);
+        debtMap.put(user3, 0.0);
+        this.testEvent= new Event("TestEvent", debtMap, user1, new SplitDebtUpdater());
         group.addEvent(testEvent);
-
-
         testEvent.updateEventMemberDebts();
+    }
 
+    @Test
+    public void testPayerPositiveDebt(){
+        List<Debt> user1List = user1.getDebtList();
 
+        for (Debt debt: user1List) {
+            assertEquals(debt.getDebtAmount(), 110.0, 1);
+        }
+    }
 
+    @Test
+    public void testUser2NegativeDebt(){
+        List<Debt> user2List = user2.getDebtList();
+        for (Debt debt: user2List) {
+            if(debt.getDebtHolder().equals(user1)){
+                assertEquals(debt.getDebtAmount(), -110.0, 1);
+            }
+        }
     }
 }
