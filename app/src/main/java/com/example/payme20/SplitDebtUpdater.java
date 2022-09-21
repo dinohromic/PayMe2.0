74173d5e5749fcb55cf3b9560cc4e17cdc0d1b92
BenjaminTWP <benjamintwpettersson@gmail.com>
2022-09-21 10:01:39 +0200

    SplitDebtUpdater now has functionality

---
+++ app/src/main/java/com/example/payme20/SplitDebtUpdater.java
@@ -0,0 +1,39 @@
+package com.example.payme20;
+
+import java.util.Map;
+
+public class SplitDebtUpdater implements IPaymentCalculation{
+    @Override
+    public void calculateEventExpenditures(Map<Member, Double> eventMemberPaidAmount, Member payer) {
+        double totalGroupCost = calcTotalGroupCost(eventMemberPaidAmount);
+        double splitCost = calcDividedCost(totalGroupCost, eventMemberPaidAmount.size());
+        updateDebt(eventMemberPaidAmount, payer, splitCost);
+    }
+
+    private double calcTotalGroupCost(Map<Member, Double> eventMemberPaidAmount) {
+        double total = 0;
+        for (Map.Entry<Member, Double> mapValue: eventMemberPaidAmount.entrySet()) {
+            total += mapValue.getValue();
+        }
+        return total;
+    }
+
+    private double calcDividedCost(double totalGroupCost, int memberSize){
+        double dividedCost = 0;
+        try{dividedCost = totalGroupCost / memberSize;}
+        catch (ArithmeticException e){System.out.println("Division by zero");}
+        return dividedCost;
+    }
+
+    private void updateDebt(Map<Member, Double> eventMemberPaidAmount, Member memberToGetPaid, double amount){
+
+        for (Map.Entry<Member, Double> debtMap: eventMemberPaidAmount.entrySet()) {
+            Member memberToPay = debtMap.getKey();
+            if(!(memberToPay.equals(memberToGetPaid))){
+                memberToGetPaid.updateDebt(amount, memberToPay);
+                memberToPay.updateDebt(-amount, memberToGetPaid);
+            }
+
+        }
+    }
+}
