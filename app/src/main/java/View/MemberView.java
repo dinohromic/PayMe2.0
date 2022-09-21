b9f6c4521c6b25fd6c5c1db852176ed26b9d308f
dinohromic <dhromic@gmail.com>
2022-09-21 15:55:44 +0200

    Ändrat lite i MemberView klassen

--- app/src/main/java/View/MemberView.java
+++ app/src/main/java/View/MemberView.java
@@ -1,4 +1,24 @@
 package View;
 
-public class MemberView {
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.payme20.Group;
+import com.example.payme20.Member;
+import com.example.payme20.R;
+
+public class MemberView extends AppCompatActivity{
+
+    private Member member;
+    private Group group;
+
+    public void onAddMemberBtnClick(View view){
+        TextView TxtName = findViewById(R.id.edtTxtName);
+        TextView TxtPhoneNumber = findViewById(R.id.editTextPhone);
+    }
+
 }
