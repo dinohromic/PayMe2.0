7a06491be873ad32a492fffb677012aea38d0c38
jihad <jihad.mohammed95@gmail.com>
2022-09-22 14:56:45 +0200

    Event view

---
+++ app/src/main/java/View/EventView.java
@@ -0,0 +1,20 @@
+package View;
+
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.payme20.Event;
+import com.example.payme20.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class EventView extends AppCompatActivity {
+    public void createEvent(){
+        TextInputLayout eventName = findViewById(R.id.eventNameInput);
+        EditText eventdate= findViewById(R.id.editEventDate);
+        RadioGroup paymentType = findViewById(R.id.payemtnType);
+        TextInputLayout payer = findViewById(R.id.Payer);
+
+    }
+}
