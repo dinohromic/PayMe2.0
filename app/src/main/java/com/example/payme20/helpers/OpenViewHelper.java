e072d7b49800d69fac6be1bb0c0ca2957b69e6ac
BenjaminTWP <benjamintwpettersson@gmail.com>
2022-09-26 19:49:20 +0200

    Mainly initiating displayitems in GroupListView

--- app/src/main/java/com/example/payme20/helpers/OpenViewHelper.java
+++ app/src/main/java/com/example/payme20/helpers/OpenViewHelper.java
@@ -3,7 +3,20 @@ package com.example.payme20.helpers;
 import android.content.Context;
 import android.content.Intent;
 
-public class OpenViewHelper {
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.payme20.MainActivity;
 
+/**
+ *Tanken bakom denna klassen är att den ska hjälpa
+ * views att öppna nya views genom att skapa intent åt dem.
+ * Detta kan skilja sig så detta skulle kunna innebära flera
+ * overloadade metoder
+ */
+
+public class OpenViewHelper  {
+
+    public void openView(Class<?> newView, Context currentView){
+        Intent newIntent = new Intent();
+    }
 }
--- app/src/main/java/com/example/payme20/views/GroupListView.java
+++ app/src/main/java/com/example/payme20/views/GroupListView.java
@@ -1,17 +1,43 @@
 package com.example.payme20.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import com.example.payme20.MainActivity;
 import com.example.payme20.R;
 
 public class GroupListView extends AppCompatActivity {
 
+    ImageButton currentGroupsReturnButton;
+    LinearLayout groupCardHolder;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_of_groups);
+        initiateClickable();
+        setOpenViewListener(this.currentGroupsReturnButton, MainActivity.class);
+    }
+
+    private void initiateClickable() {
+        this.currentGroupsReturnButton = findViewById(R.id.currentGorupsReturnButton);
+        this.groupCardHolder = findViewById(R.id.groupListLinearLayout);
+    }
+
+    private void setOpenViewListener(ImageView openButton, Class<?> viewToOpen){
+        openButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(GroupListView.this, viewToOpen);
+                GroupListView.this.startActivity(myIntent);
+            }
+        });
     }
 
 }
