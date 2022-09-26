c165da8a55b6a08e967893dca01564dbbd2a9b54
BenjaminTWP <benjamintwpettersson@gmail.com>
2022-09-26 11:47:02 +0200

    Annoying bar at the top removed, MainActivity got navigation, GroupList and GroupCreate now has a View-class. Might have changed color to green :3

---
+++ app/src/main/java/com/example/payme20/helpers/OpenViewHelper.java
@@ -0,0 +1,9 @@
+package com.example.payme20.helpers;
+
+import android.content.Context;
+import android.content.Intent;
+
+public class OpenViewHelper {
+
+
+}
---
+++ app/src/main/java/com/example/payme20/views/GroupListView.java
@@ -0,0 +1,17 @@
+package com.example.payme20.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.payme20.R;
+
+public class GroupListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_of_groups);
+    }
+
+}
