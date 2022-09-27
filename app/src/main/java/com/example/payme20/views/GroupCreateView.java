e1ed2e8bf435d148f05a138b6c542d00b56d59e6
jihad <jihad.mohammed95@gmail.com>
2022-09-27 11:32:38 +0200

    Group name error

--- app/src/main/java/com/example/payme20/views/GroupCreateView.java
+++ app/src/main/java/com/example/payme20/views/GroupCreateView.java
@@ -1,26 +1,39 @@
 package com.example.payme20.views;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.payme20.R;
 
 public class GroupCreateView extends AppCompatActivity {
+    private EditText groupName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_group);
         Button CreateGroupButton = findViewById(R.id.createGroupView);
+        groupName= findViewById(R.id.groupNameTextView);
         CreateGroupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openAddMembersPage();
+                if(TextUtils.isEmpty(groupName.getText().toString())){
+                    Toast.makeText(GroupCreateView.this,"Group name needed", Toast.LENGTH_SHORT);
+                }
+                else {
+                    openAddMembersPage();
+                }
             }
         });
     }
@@ -29,6 +42,8 @@ public class GroupCreateView extends AppCompatActivity {
         Intent intent = new Intent(GroupCreateView.this, MemberView.class);
         GroupCreateView.this.startActivity(intent);
     }
+    public void onAddMemberBtnClick(View view){
 
-        //EditText groupName= findViewById(R.id.groupNameTextView);
+    }
+
 }
