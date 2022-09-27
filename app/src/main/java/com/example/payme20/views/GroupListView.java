f2508cdea10bbfefa3f1dc505a2ec14191427575
oscarkarbin <karbin.oscar2002@gmail.com>
2022-09-27 15:25:22 +0200

    Dynamic cards are now working for list_of_groups

--- app/src/main/java/com/example/payme20/views/GroupListView.java
+++ app/src/main/java/com/example/payme20/views/GroupListView.java
@@ -6,17 +6,23 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import com.example.payme20.MainActivity;
 import com.example.payme20.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class GroupListView extends AppCompatActivity {
 
     ImageButton currentGroupsReturnButton;
     LinearLayout groupCardHolder;
+    FloatingActionButton createGroupFAB;
+    CardView groupCardView;
+    LinearLayout container;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -24,13 +30,29 @@ public class GroupListView extends AppCompatActivity {
         initiateClickable();
         setOpenViewListener(this.currentGroupsReturnButton, MainActivity.class);
 
+        createGroupFAB = findViewById(R.id.createGroupFAB);
+        container = findViewById(R.id.groupListContainer);
+        createGroupFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addCard("Test");
+            }
+        });
 
 
+    }
+    private void addCard(String name) {
+        View view = getLayoutInflater().inflate(R.layout.group_card_view, null);
+
+        TextView groupName = view.findViewById(R.id.activeGroupsName);
+
+        groupName.setText(name);
+        container.addView(view);
     }
 
     private void initiateClickable() {
         this.currentGroupsReturnButton = findViewById(R.id.currentGorupsReturnButton);
-        this.groupCardHolder = findViewById(R.id.groupListLinearLayout);
+        this.groupCardHolder = findViewById(R.id.groupListContainer);
     }
 
     private void setOpenViewListener(ImageView openButton, Class<?> viewToOpen){
