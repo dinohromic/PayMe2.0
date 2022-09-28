f0ac29aac6963a7277472ef364429b507f833291
antonboras <boras@net.chalmers.se>
2022-09-28 09:59:05 +0200

    Split av kostnad distribuerar random ut resten

--- app/src/main/java/com/example/payme20/model/SplitDebtUpdater.java
+++ app/src/main/java/com/example/payme20/model/SplitDebtUpdater.java
@@ -1,15 +1,22 @@
 package com.example.payme20.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class SplitDebtUpdater implements IDebtUpdater {
     @Override
     public List<Debt> updateDebts(Map<Member, Integer> eventMemberPaidAmount, Member payer) {
         int totalGroupCost = calcTotalGroupCost(eventMemberPaidAmount);
         int splitCost = calcDividedCost(totalGroupCost, eventMemberPaidAmount.size());
-        return createEventDebtList(eventMemberPaidAmount, payer, splitCost);
+        List<Member> membersList = new ArrayList<>();
+        for (Map.Entry<Member, Integer> debtMap: eventMemberPaidAmount.entrySet()) {
+            membersList.add(debtMap.getKey());
+        }
+        Map<Member, Integer> updatedMap = distributeRest(totalGroupCost, splitCost, eventMemberPaidAmount, membersList);
+        return createEventDebtList(payer, updatedMap);
     }
 
     private int calcTotalGroupCost(Map<Member, Integer> eventMemberPaidAmount) {
@@ -26,13 +33,32 @@ public class SplitDebtUpdater implements IDebtUpdater {
         catch (ArithmeticException e){System.out.println("Division by zero in SplitDebtUpdater.Class");}
         return dividedCost;
     }
+    private Map<Member, Integer> distributeRest(int totalGroupCost, int dividedcost, Map<Member, Integer> map, List<Member> membersList) {
+        int memberSize = map.size();
+        int rest = totalGroupCost - dividedcost*memberSize;
+        Map<Member, Integer> updatedMap = new HashMap<>();
+        Random r = new Random();
+        List<Integer> indexList = new ArrayList<>();
+        for (Map.Entry<Member, Integer> debtMap: map.entrySet()) {
+            updatedMap.put(debtMap.getKey(), dividedcost);
+        }
+        while(indexList.size() < rest) {
+            int index = r.nextInt(memberSize);
+            if(!indexList.contains(index))
+                indexList.add(index);
+        }
+        for(Integer i : indexList) {
+            updatedMap.put(membersList.get(i), dividedcost + 1);
+        }
+        return updatedMap;
+    }
 
-    private List<Debt> createEventDebtList(Map<Member, Integer> eventMemberPaidAmount, Member memberToGetPaid, int debtAmount){
+    private List<Debt> createEventDebtList(Member memberToGetPaid, Map<Member, Integer> updatedMap){
         List<Debt> eventDebtList = new ArrayList<>();
-        for (Map.Entry<Member, Integer> debtMap: eventMemberPaidAmount.entrySet()) {
+        for (Map.Entry<Member, Integer> debtMap: updatedMap.entrySet()) {
             Member memberToPay = debtMap.getKey();
             if(!(memberToPay.equals(memberToGetPaid))){
-                eventDebtList.add(new Debt(memberToGetPaid, memberToPay, debtAmount));
+                eventDebtList.add(new Debt(memberToGetPaid, memberToPay, debtMap.getValue()));
             }
         }
         return eventDebtList;
