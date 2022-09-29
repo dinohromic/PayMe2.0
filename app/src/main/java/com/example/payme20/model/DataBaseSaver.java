0c0ce61db1abb44dd19e5b260c54a8f803a309a5
dinohromic <dhromic@gmail.com>
2022-09-29 14:11:41 +0200

    Har fixat med att lägga till members i SQLite och samt radera members. Sen ett försök på en metod som tar in alla members och returnerar en lista av de.

--- app/src/main/java/com/example/payme20/model/DataBaseSaver.java
+++ app/src/main/java/com/example/payme20/model/DataBaseSaver.java
@@ -2,14 +2,18 @@ package com.example.payme20.model;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
 
 import androidx.annotation.Nullable;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataBaseSaver extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
@@ -44,19 +48,50 @@ public class DataBaseSaver extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_MEMBER);
 
+        onCreate(sqLiteDatabase);
     }
 
     public void addGroupMembers(Member member ){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
-        //if( GroupCreate == true) CREATE TABLE
+        // for(int i = 0, i < members, i ++){
+        //      if( GroupCreate == true) CREATE TABLE;
         values.put(KEY_NAME, member.getUserName());
         values.put(KEY_NUMBER, member.getPhoneNumber());
         db.insert(TABLE_MEMBER, null, values);
         db.close();
 
     }
+
+    public void deleteMember(Member member){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        sqLiteDatabase.delete(TABLE_MEMBER, KEY_NAME +" =?", new String[]{String.valueOf(member.getUserName())});
+
+    }
+
+    //Return to this method, the parameters are wrong. They are only set there for "new Member()"s satisfaction
+    // Another problem is that the if statement is wrong because this method needs the the list of the members from ViewModel
+    public List<Member> getAllMembers(String userName, String phoneNumber){
+        List<Member> memberList = new ArrayList<Member>();
+        String selectQuery = "SELECT *FROM " +  TABLE_MEMBER;
+
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()){
+            do{
+                Member member = new Member(userName,phoneNumber);
+                member.setUserName(cursor.getString(0));
+                member.setPhoneNumber(cursor.getString(1));
+                memberList.add(member);
+            } while (cursor.moveToNext());
+        }
+        return memberList;
+    }
+
+
 
 
 }
