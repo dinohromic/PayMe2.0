e8fe8b5be91719c9ed3efc975b397a59b7cb4e10
jihad <jihad.mohammed95@gmail.com>
2022-09-30 12:57:51 +0200

    Fixed issue with things disappearing when keyboard appeared on screen

--- app/src/main/java/com/example/payme20/ViewModels/GroupListViewModel.java
+++ app/src/main/java/com/example/payme20/ViewModels/GroupListViewModel.java
@@ -18,8 +18,14 @@ public class GroupListViewModel {
         List<Member> members = new ArrayList<>();
         Member test1 = new Member("Arne", "112");
         Member test2 = new Member("Anders", "114 14");
+        Member test3 = new Member("ahmed","231");
+        Member test4 = new Member("Jihad","231");
+
         members.add(test1);
         members.add(test2);
+        members.add(test3);
+        members.add(test4);
+
         test.add(new Group("Grekland", members));
         test.add(new Group("Middag", members));
         return test;
