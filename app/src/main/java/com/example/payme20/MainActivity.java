d74218bb2536f39b1085181d6ee29ccffdd70636
jihad <jihad.mohammed95@gmail.com>
2022-09-30 11:17:57 +0200

    Implemented Light and Dark mode to the app

--- app/src/main/java/com/example/payme20/MainActivity.java
+++ app/src/main/java/com/example/payme20/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.payme20;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
+
 import com.example.payme20.helpers.OpenViewHelper;
 import com.example.payme20.views.GroupListView;
 import com.example.payme20.views.MemberView;
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     
     Button openGroupListButton;
     Button createGroupButton;
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +26,23 @@ public class MainActivity extends AppCompatActivity {
         initiateClickable();
         setOpenViewListener(this.openGroupListButton, GroupListView.class);
         setOpenViewListener(this.createGroupButton, MemberView.class);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.idRBLight:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        break;
+                    case R.id.idRBDark:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        break;
+                }
+            }
+    });
     }
 
     private void initiateClickable(){
+        radioGroup = findViewById(R.id.idRGgroup);
         this.openGroupListButton = findViewById(R.id.groupPageButton);
         this.createGroupButton = findViewById(R.id.addMemberButton);
     }
