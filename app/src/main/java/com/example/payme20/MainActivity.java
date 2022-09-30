16d237a26099f37bf7019c4fe8dd1aaea2847efe
jihad <jihad.mohammed95@gmail.com>
2022-09-30 14:53:07 +0200

    Reimplemented Light and Dark mode with switch instead of radio buttons

--- app/src/main/java/com/example/payme20/MainActivity.java
+++ app/src/main/java/com/example/payme20/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.payme20;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.example.payme20.helpers.OpenViewHelper;
@@ -17,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     
     Button openGroupListButton;
     Button createGroupButton;
-    private RadioGroup radioGroup;
+    private SwitchCompat switchCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +30,22 @@ public class MainActivity extends AppCompatActivity {
         initiateClickable();
         setOpenViewListener(this.openGroupListButton, GroupListView.class);
         setOpenViewListener(this.createGroupButton, MemberView.class);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        switchCompat = findViewById(R.id.switch1);
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId) {
-                    case R.id.idRBLight:
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                        break;
-                    case R.id.idRBDark:
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                        break;
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+                else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 }
             }
-    });
+        });
     }
 
     private void initiateClickable(){
-        radioGroup = findViewById(R.id.idRGgroup);
+
         this.openGroupListButton = findViewById(R.id.groupPageButton);
         this.createGroupButton = findViewById(R.id.addMemberButton);
     }
