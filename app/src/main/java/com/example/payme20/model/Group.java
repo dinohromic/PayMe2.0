a3d502c058558a6f5cf0464dd734aada19e5e005
antonboras <boras@net.chalmers.se>
2022-10-04 11:50:04 +0200

    vid 0 i amount i create event så läggs man inte till

--- app/src/main/java/com/example/payme20/model/Group.java
+++ app/src/main/java/com/example/payme20/model/Group.java
@@ -66,7 +66,7 @@ public class Group implements Serializable {
     public void addEvent(Event event) {
         groupEvents.add(event);
         addEventDebtToGroup(event.getDebtList());
-        System.out.println(this.groupEvents);
+        System.out.println(this.groupEvents.size());
     }
 
     public void setAllEventsInactive(){
