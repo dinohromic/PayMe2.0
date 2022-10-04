cc60118c0fd09c573bec669adc210f42194d0797
dinohromic <dhromic@gmail.com>
2022-10-04 21:48:44 +0200

    I MemberDaoImplement har jag lagt till samma metoder som finns med i DataBaseSaver. Tanken i DAO är att de ska ligga här istället.

--- app/src/main/java/com/example/payme20/model/MemberDAO.java
+++ app/src/main/java/com/example/payme20/model/MemberDAO.java
@@ -1,10 +1,13 @@
 package com.example.payme20.model;
 
+import java.util.List;
+
 public interface MemberDAO {
     public void addGroupMembers(Member member);
     public void getMemberFromDB(int id);
     public void deleteMembers(Member member);
     public int getCountAllMembers();
+    public List<Member> getALlMembersFromDB();
 
 
 }
--- app/src/main/java/com/example/payme20/model/MemberDaoImplement.java
+++ app/src/main/java/com/example/payme20/model/MemberDaoImplement.java
@@ -1,6 +1,29 @@
 package com.example.payme20.model;
 
-public class MemberDaoImplement implements MemberDAO {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class MemberDaoImplement extends SQLiteOpenHelper implements MemberDAO {
+
+
+    public MemberDaoImplement(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 
     @Override
     public void addGroupMembers(Member member) {
@@ -21,4 +44,11 @@ public class MemberDaoImplement implements MemberDAO {
     public int getCountAllMembers() {
         return 0;
     }
+
+    @Override
+    public List<Member> getALlMembersFromDB() {
+        return null;
+    }
+
+
 }
