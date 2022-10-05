a6da4de01f73366bdf5a554884f40e9ccc24dbc7
BenjaminTWP <benjamintwpettersson@gmail.com>
2022-10-05 11:32:42 +0200

    Event.java and Member.java commented with javadoc comments

--- app/src/main/java/com/example/payme20/model/Event.java
+++ app/src/main/java/com/example/payme20/model/Event.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
+/**
+ *
+ */
 public class Event implements Serializable {
     private final Map<Member, Integer> eventPaymentDetails;
     private final IDebtUpdater debtUpdater;
@@ -15,6 +18,14 @@ public class Event implements Serializable {
     private final String date;
 
 
+    /**
+     * Create a new event
+     * @param eventName is the name of the event
+     * @param eventPaymentDetails is a map containing a member as a key and an integer as the value
+     * @param payer the member who paid for the event
+     * @param debtUpdater the method of paying for the event
+     * @param date the date the event took place
+     */
     public Event(String eventName, Map eventPaymentDetails, Member payer, IDebtUpdater debtUpdater, String date){
         this.eventName=eventName;
         this.eventPaymentDetails = eventPaymentDetails;
@@ -23,33 +34,58 @@ public class Event implements Serializable {
         this.debtUpdater = debtUpdater;
         this.date = date;
         this.eventDebtList = createEventDebts();
-
     }
 
+    /**
+     * Return the map with payment-details from the event
+     * @return returns a map containing members as a key and values as integers
+     */
     public Map<Member, Integer> getEventPaymentDetails() {
         return eventPaymentDetails;
     }
 
+    /**
+     * Get the status of the events activity
+     * @return returns a boolean, true if event is active and false if event i inactive
+     */
     public boolean getActiveStatus() {
         return activeStatus;
     }
 
+    /**
+     * Set the event as inactive
+     */
     public void setEventInactive(){
         this.activeStatus = false;
     }
 
+    /**
+     * Set the event as active
+     */
     public void setEventActive(){
         this.activeStatus = true;
     }
 
+    /**
+     * Get the member who paid for the event
+     * @return returns a Member-object
+     */
     public Member getPayer() {
         return payer;
     }
 
+    /**
+     * Get the list of debts that the event holds
+     * @return the list containing Debt-objects
+     */
     public List<Debt> getDebtList(){
         return this.eventDebtList;
     }
 
+    /**
+     * Creates the list of debts
+     * @return returns a created list of Debt-objects
+     */
     private List<Debt> createEventDebts() {
         return debtUpdater.updateDebts(eventPaymentDetails, payer);
 
@@ -69,14 +105,27 @@ public class Event implements Serializable {
                 '}';
     }
 
+
+    /**
+     * Get the name of the event
+     * @return returns a string of the events name
+     */
     public String getEventName() {
         return this.eventName;
     }
 
+    /**
+     * Get the date that the event took place
+     * @return returns a string with the date
+     */
     public String getEventDate() {
         return this.date;
     }
 
+    /**
+     * Get the debtUpdater this class used
+     * @return returns an object that implements IDebtUpdater
+     */
     public IDebtUpdater getdebtUpdater() {
         return this.debtUpdater;
     }
--- app/src/main/java/com/example/payme20/model/Member.java
+++ app/src/main/java/com/example/payme20/model/Member.java
@@ -1,16 +1,23 @@
 package com.example.payme20.model;
 
 import androidx.annotation.NonNull;
-
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * A member has a username, a phone number and is identified with a unique ID
+ */
 public class Member implements Serializable {
     private int id;
     private String userName;
     private String phoneNumber;
 
-
+    /**
+     * Create a new member
+     * @param userName Create the member with the given user name
+     * @param phoneNumber Create the member with the given phone number
+     * @param id Create the member with the given ID
+     */
     public Member(String userName, String phoneNumber, int id){
 
         this.userName = userName;
@@ -18,27 +25,50 @@ public class Member implements Serializable {
         this.id = id;
     }
 
+    /**
+     * Get the user name of a member
+     * @return returns the username of the member called upn
+     */
     public String getUserName() {
         return userName;
     }
 
-
+    /**
+     * Get the phone number of a member
+     * @return returns the phone number of the member called upon
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Set the user name of a member
+     * @param userName the new user name of the member
+     */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**
+     * Set the phone number of a member
+     * @param phoneNumber the new phone number of the member
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * Get the ID of a member
+     * @return returns the ID of the member called upon
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Set the ID of a member
+     * @param id the new ID of the member
+     */
     public void setId(int id) {
         this.id = id;
     }
