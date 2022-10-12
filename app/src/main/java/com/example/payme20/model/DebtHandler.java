eb5ff81100dcac2e3f54e3460e817d42f5a388b7
jihad <jihad.mohammed95@gmail.com>
2022-10-12 09:48:56 +0200

    Commented and small fixes to DebtHandler class

--- app/src/main/java/com/example/payme20/model/DebtHandler.java
+++ app/src/main/java/com/example/payme20/model/DebtHandler.java
@@ -10,6 +10,10 @@ public class DebtHandler implements Serializable {
     private Map<Member, List<Debt>> incomingDebtsMaps = new HashMap<>();
     private Map<Member, List<Debt>> outgoingDebtsMap = new HashMap<>();
 
+    /**
+     * Create new debt list if the current debt lists are empty, then adds the debt to both from and to debt maps.
+     * @param debt the debt object to be added
+     */
     public void addDebt(Debt debt) {
         List<Debt> toDebts = incomingDebtsMaps.get(debt.getDebtTo());
         List<Debt> fromDebts = outgoingDebtsMap.get(debt.getDebtFrom());
@@ -25,17 +29,35 @@ public class DebtHandler implements Serializable {
         outgoingDebtsMap.put(debt.getDebtFrom(), fromDebts);
     }
 
+    /**
+     * get a members incoming debts
+     * @return a map with the member and a list of debts
+     */
     public Map<Member, List<Debt>> getIncomingDebtsMaps() {
         return incomingDebtsMaps;
     }
 
+    /**
+     * get a members outgoing debts
+     * @return a map with the member and a list of debts
+     */
     public Map<Member, List<Debt>> getOutgoingDebtsMap() {
         return outgoingDebtsMap;
     }
 
+    /**
+     * if the current debt lists are empty, then remove the debt from both from and to debt maps.
+     * @param debt the debt object to be removed
+     */
     public void removeDebt(Debt debt) {
         List<Debt> toDebts = incomingDebtsMaps.get(debt.getDebtTo());
         List<Debt> fromDebts = outgoingDebtsMap.get(debt.getDebtFrom());
+        if(toDebts == null) {
+            toDebts = new ArrayList<>();
+        }
+        if(fromDebts == null) {
+            fromDebts = new ArrayList<>();
+        }
         toDebts.remove(debt);
         fromDebts.remove(debt);
         incomingDebtsMaps.put(debt.getDebtTo(), toDebts);
