20e734fa5bc97848a137a56f350a8ac4fee7934c
dinohromic <dhromic@gmail.com>
2022-10-13 07:34:37 +0200

    Ändrade lite i deserialize metoden. Det som behövs fixas är att itterera över mappen av eventPaymentDetails och listan eventDebtList

--- app/src/main/java/com/example/payme20/model/GroupDeserializer.java
+++ app/src/main/java/com/example/payme20/model/GroupDeserializer.java
@@ -1,5 +1,7 @@
 package com.example.payme20.model;
 
+import android.icu.text.Edits;
+
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.ObjectCodec;
@@ -9,8 +11,11 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class GroupDeserializer extends StdDeserializer<Group> {
@@ -46,10 +51,22 @@ public class GroupDeserializer extends StdDeserializer<Group> {
                 groupMembers.add(new Member(userName, phoneNumber));
             }
             //Gör för events också
-
-
-
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try{
+            JsonNode nameOfNode = node.get("group_name");
+            name = nameOfNode.asText();
+            JsonNode event = node.get("events");
+            for (int i = 0; i < event.size(); i++) {
+                String eventName = event.get(i).get("eventName").asText();
+                String payer = event.get(i).get("payer").asText();
+                boolean activeEvent = event.get(i).get("activeEvent").asBoolean();
+                String date = event.get(i).get("date").asText();
+                //Fixa att iterera över en MAP med eventPaymentDetails sedan över eventDebtList.
+            }
+            } catch (Exception e){
             e.printStackTrace();
         }
         return new Group(name, groupMembers);
