53edcc8b992cdedc3eb5fcfc39708032811ec598
dinohromic <dhromic@gmail.com>
2022-10-13 13:31:23 +0200

    Tog bort onödig kod i MemberSerializer

--- app/src/main/java/com/example/payme20/model/MemberSerializer.java
+++ app/src/main/java/com/example/payme20/model/MemberSerializer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.google.gson.JsonParser;
 
 import java.io.IOException;
 import java.io.StringWriter;
