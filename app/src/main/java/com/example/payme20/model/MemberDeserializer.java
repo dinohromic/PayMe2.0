247da4c89eb2c8a8bb6e9159f3d4c540e492b6a8
dinohromic <dhromic@gmail.com>
2022-10-13 13:27:31 +0200

    Serialisering av Members, Groups och Event. Gjort så att det funkar att spara groups i current group filen. Samt lyckats spara event i gruppsidan
    Co-authored-by: antonboras boras@net.chalmers.se

---
+++ app/src/main/java/com/example/payme20/model/MemberDeserializer.java
@@ -0,0 +1,13 @@
+package com.example.payme20.model;
+
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.KeyDeserializer;
+
+import java.io.IOException;
+
+public class MemberDeserializer extends KeyDeserializer {
+    @Override
+    public Member deserializeKey(String key, DeserializationContext ctxt) throws IOException {
+        return new Member(key);
+    }
+}
---
+++ app/src/main/java/com/example/payme20/model/MemberSerializer.java
@@ -0,0 +1,22 @@
+package com.example.payme20.model;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class MemberSerializer extends JsonSerializer<Member> {
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+    @Override
+    public void serialize(Member value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        StringWriter write = new StringWriter();
+        mapper.writeValue(write, value.toString());
+        gen.writeFieldName(write.toString());
+    }
+}
