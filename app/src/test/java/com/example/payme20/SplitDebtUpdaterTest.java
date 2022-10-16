24babcd3620f577993ce4a12c99764f06e7f4a53
BenjaminTWP <benjamintwpettersson@gmail.com>
2022-10-16 10:38:17 +0200

    SplitDebtUpdaterTest.java all tests work. All tests in all classes runs as before

--- app/src/test/java/com/example/payme20/SplitDebtUpdaterTest.java
+++ app/src/test/java/com/example/payme20/SplitDebtUpdaterTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import com.example.payme20.model.DebtCalculator;
 import com.example.payme20.model.Event;
 import com.example.payme20.model.Group;
 import com.example.payme20.model.Member;
@@ -16,43 +17,43 @@ import java.util.Map;
 
 public class SplitDebtUpdaterTest {
 
+    Map<Member, Integer> debtMap = new HashMap<>();
+    DebtCalculator debtCalculator = new DebtCalculator();
+    Event testEvent;
     Member user1;
     Member user2;
     Member user3;
     Group group;
-    Map<Member, Integer> debtMap = new HashMap<Member, Integer>();
-    Event testEvent;
-    PayMeModel payMeModel = PayMeModel.INSTANCE;
 
     @Before
     public void init(){
-        user1 = new Member("User1", "100", -1);
-        user2 = new Member("User2", "200", -1);
-        user3 = new Member("User3", "300", -1);
-        group = new Group("TestGroup", new ArrayList<>());
-        group.addNewGroupMember(user1);
-        group.addNewGroupMember(user2);
-        group.addNewGroupMember(user3);
-        debtMap.put(user1, 332);
-        debtMap.put(user2, 0);
-        debtMap.put(user3, 0);
+        this.user1 = new Member("User1", "100", -1);
+        this.user2 = new Member("User2", "200", -2);
+        this.user3 = new Member("User3", "300", -3);
+        this.group = new Group("TestGroup", new ArrayList<>());
+        this.group.addNewGroupMember(user1);
+        this.group.addNewGroupMember(user2);
+        this.group.addNewGroupMember(user3);
+        this.debtMap.put(user1, 332);
+        this.debtMap.put(user2, 0);
+        this.debtMap.put(user3, 0);
         this.testEvent= new Event("TestEvent", debtMap, user1, new SplitCreateDebtList(), "");
-        group.addEvent(testEvent);
+        this.group.addEvent(testEvent);
     }
+
     @Test
     public void testPayerPositiveDebt(){
-        System.out.println(payMeModel.getTotalDebt(group.getGroupName(), user1));
-        System.out.println(payMeModel.getTotalDebt(group.getGroupName(), user2));
-        System.out.println(payMeModel.getTotalDebt(group.getGroupName(), user3));
-        int user1TotalDebt = -(payMeModel.getTotalDebt(group.getGroupName(), user2) + payMeModel.getTotalDebt(group.getGroupName(), user2));
-        assertEquals(user1TotalDebt, payMeModel.getTotalDebt(group.getGroupName(),user1), 1); // Hur testa detta?
+        int user1TotalDebt = -(debtCalculator.calcMemberTotalDebt(this.user2, group.getDebtHandler())) - debtCalculator.calcMemberTotalDebt(this.user3, group.getDebtHandler());
+        assertEquals(user1TotalDebt, debtCalculator.calcMemberTotalDebt(this.user1, group.getDebtHandler()), 1);
     }
+
+
     @Test
     public void testUser2NegativeDebt(){
-        assertEquals(-110, payMeModel.getTotalDebt(group.getGroupName(), user2), 1);
+        assertEquals(-110, debtCalculator.calcMemberTotalDebt(this.user2, group.getDebtHandler()), 1);
     }
     @Test
     public void testUser3NegativeDebt(){
-        assertEquals(-110, payMeModel.getTotalDebt(group.getGroupName(), user3), 1);
+       assertEquals(-110, debtCalculator.calcMemberTotalDebt(this.user3, group.getDebtHandler()), 1);
     }
 }
